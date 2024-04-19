faa18500d625239a1de95e708258c5cd51bb7a76
GiulianoWF <Giuliano.Floriani@gmail.com>
2024-04-19 03:26:09 +0100

    Add first parser call blueprint

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,21 @@ use std::env;
 
 mod language_server_extender;
 use crate::language_server_extender::LanguageServerExtender;
+use crate::language_server_extender::ParsedMessage;
 
 fn main() {
     let args: Vec<String> = env::args().skip(1).collect();
 
-    // /home/dechog/.dotfiles/Code/User/globalStorage/llvm-vs-code-extensions.vscode-clangd/install/17.0.3/clangd_17.0.3/bin/clangd
-    //let mut clangd = Command::new("clangd-15")
-    let server = LanguageServerExtender::new(
+    let mut server = LanguageServerExtender::new(
         //"/home/dechog/.dotfiles/Code/User/globalStorage/llvm-vs-code-extensions.vscode-clangd/install/17.0.3/clangd_17.0.3/bin/clangd",
         "clangd-15",
         &args)
             .expect("Error running server");
+
+        server.add_handler("codeAction", Box::new(|_msg: &ParsedMessage| -> Result<String, std::io::Error> {
+            Ok("".to_string())
+        }));
+
         
         server.run();
 }
